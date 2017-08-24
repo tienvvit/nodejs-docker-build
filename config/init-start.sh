a2eeb236743abef6260ba8b71db0cb65edb8a477
root <tienvv.it@gmail.com>
2017-08-24 03:32:18 +0000

    fix bug create postgres db

--- config/init-start.sh
+++ config/init-start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 export HOME=/home/jenkins
 
-service postgresql start
 service mysql start
+service postgresql start
+
+if [ ! -f /home/jenkins/.pgpass ]; then
+   su - postgres -c "psql -c \"CREATE USER jenkins WITH PASSWORD 'secret';\""
+   su - postgres -c "psql -c \"ALTER USER jenkins CREATEDB;\""
+   echo "localhost:*:*:jenkins:secret" > /home/jenkins/.pgpass
+   chmod 0600 /home/jenkins/.pgpass
+fi
 
 while true; do sleep 1d; done
 
